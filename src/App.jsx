a9b6f2c49b399c50cb120ed13b71250bf520f8a0
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-11-29 19:00:26 +0100

    feat: create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <main className="bg-slate-300/20">
       <Router>
-        Navbar
+        <Navbar />
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
           <Route path="/about" element={<h1>About</h1>} />
