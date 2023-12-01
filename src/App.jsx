44b7b6fe833b39a5ab64e1b08395c533f4c12a50
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-12-01 19:59:06 +0100

    feat: create all pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
           <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/about" element={<h1>Projects</h1>} />
-          <Route path="/about" element={<h1>Contact</h1>} />
+          <Route path="/projects" element={<h1>Projects</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
         </Routes>
       </Router>
     </main>
