6b6976e429327b5fb04ba2d4e3ffbc6349932bb8
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-12-02 12:39:40 +0100

    feat: implement bird sky and plane on home page

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,5 +1,14 @@
+import birdScene from "../assets/3d/bird.glb";
+import { useGLTF } from "@react-three/drei";
+
 const Bird = () => {
-  return <div>Bird</div>;
+  const { scene, animations } = useGLTF(birdScene);
+
+  return (
+    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
+      <primitive object={scene} />
+    </mesh>
+  );
 };
 
 export default Bird;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Loader from "../components/Loader";
 import Island from "../models/Island";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
+import Plane from "../models/Plane";
 
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
