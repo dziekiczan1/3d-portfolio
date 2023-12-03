59ffbc9d92e8df40d4503327691f19dd3f7c2b21
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-12-03 19:08:47 +0100

    feat: add content to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,25 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Peter
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          I am a self-taught person and I derive my knowledge from people more
+          experienced in the industry. I currently live in Warsaw, and I focus
+          all my free time on developing my skills and learning about new
+          technologies that will make my projects even better.
+        </p>
+      </div>
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
+      </div>
+    </section>
+  );
 };
 
 export default About;
